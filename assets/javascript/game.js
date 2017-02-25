0704c1c3bc03e6b940e2adf090b622d8e152633f
bl3d <mr.brentlewis@gmail.com>
2017-02-24 22:08:30 -0500

    initial file structure

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict'; 
+
+
+
+	//main game object/logic
+	var trivia = {
+
+	   init: function(){
+	   		console.log('Ready to go!');
+	   }
+
+	};
+
+
+	// kick off game 
+	trivia.init(); 
+
+
+
+	//user events
+	//**************************************************//
+
+
+
+
+}()); 
